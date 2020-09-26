89053a4443725371179c4a54e4fea26df18d3e3f
sommelierr <blaze_ing@mail.ru>
2020-09-26 13:02:10 +0300

    index added

--- src/main/java/by/s0mmelier/controllers/HomeController.java
+++ src/main/java/by/s0mmelier/controllers/HomeController.java
@@ -31,10 +31,18 @@ public class HomeController {
     @GetMapping("/home")
     public HomeDto getHome(){
         HomeDto homeData = new HomeDto();
-        homeData.setAlcohol(alcoholService.getLast());
-        homeData.setBook(bookService.getLast());
-        homeData.setTags(tagService.toCloudTags(tagService.getAlltags()));
-        homeData.setCollection(collectionService.getBiggestCollection());
+        if(alcoholService.getLast() != null) {
+            homeData.setAlcohol(alcoholService.getLast());
+        }
+        if(bookService.getLast() != null) {
+            homeData.setBook(bookService.getLast());
+        }
+        if(tagService.getAlltags() !=null) {
+            homeData.setTags(tagService.toCloudTags(tagService.getAlltags()));
+        }
+        if(collectionService.getBiggestCollection() != null) {
+            homeData.setCollection(collectionService.getBiggestCollection());
+        }
         return homeData;
     }
 }
